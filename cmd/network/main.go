049f3beabc272a644b0773f161caf59f438047cd
Gilles Lenaerts <gilleslenaerts007@gmail.com>
2024-02-28 15:47:53 +0100

    add const variables as example
    Signed-off-by: Gilles Lenaerts <gilleslenaerts007@gmail.com>

--- cmd/network/main.go
+++ cmd/network/main.go
@@ -12,9 +12,14 @@ const (
 	defgateway = "10.0.0.1"  // Gateway you want to set
 	prefix_nr  = 20          // Prefix number
 	ip_mode    = "manual"    // Mode you want to set
+
+	ethInterfaceName = "eth0"
+	ConnectionID     = "Wired connection 1"
 )
 
 func Setup_EthInterface() (error, string) {
+	network.SetupEthInterface(ethInterfaceName, ConnectionID)
+
 	err := network.SetIPAddr(ip_addr, prefix_nr, defgateway)
 	if err != nil {
 		log.Fatal(err)
